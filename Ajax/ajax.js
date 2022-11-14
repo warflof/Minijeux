a898ffad4c4ba4291442ce898a145b2880b7468a
Warflof <l.richard.developpement@gmail.com>
2022-11-14 11:54:20 +0100

    add pages Ajax

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -1,35 +1,59 @@
 function ShowUsers(){
-    const request = new XMLHttpRequest();
-    //Appel la requête JS
-    request.open('GET', 'https://reqres.in/api/users?page=1');
+  const nbPages = document.getElementById('pages').value;
+  getUsers(nbPages);
 
-    //Event d'appel au clic du bouton
-    request.addEventListener('readystatechange', function() {
-        //Si le lien est ok et que tout est envoyé, alors on affiche les données
-        if(request.readyState === 4) {
-              if(request.status === 200) {
-                  console.log("Response = " + request.response);
-                  const object = JSON.parse(request.response);
-                  
-                  let myhtml = "";
+};
+
+/*
+            let myhtml = "";
                   object.data.forEach(element => {
 
                     myhtml += '<div><img src="'+element.avatar+'"/><div><p>'+element.first_name+' '
                               +element.last_name+'</p></div>'
+
+                  document.getElementById("AllUsers").innerHTML = myhtml;
                     
                   });
-                  document.getElementById("AllUsers").innerHTML = myhtml;
-              }
-              else if(request.status === 301) {
-                alert("L'url de votre requête n'est pas bonne");
-              }
-              else{
-                alert('Une erreur est survenue');
-              }
-            console.log("Response = " + request.response);
-        };
-    });
-    request.send();
-};
+*/
+function getUsers(numeroPage){
+  const request = new XMLHttpRequest();
+  const url = 'https://reqres.in/api/users?page='+numeroPage
+  request.open('GET', url);
 
+  request.addEventListener('readystatechange', function() {
+    //Si le lien est ok et que tout est envoyé, alors on affiche les données
+    if(request.readyState === 4) {
+          if(request.status === 200) {
+              console.log("Response = " + request.response);
+              const object = JSON.parse(request.response);
+              setUsersInPages(object);
+              
+             
+              
+          }
+          else if(request.status === 301) {
+            alert("L'url de votre requête n'est pas bonne");
+          }
+          else{
+            alert('Une erreur est survenue');
+          }
+        console.log("Response = " + request.response);
+    };
+});
+
+    request.send();
+
+}
+
+function setUsersInPages(object){
+  let myhtml = "";
+     object.data.forEach(element => {
+
+      myhtml += '<div><img src="'+element.avatar+'"/><div><p>'+element.first_name+' '
+                 +element.last_name+'</p></div>'
+      
+                 document.getElementById("AllUsers").innerHTML = myhtml;
+                    
+      });
+}
 
